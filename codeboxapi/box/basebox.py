4342ec9309d16925cc1e05f69874489bec7f11ad
Shroominic <db2002dominic@gmail.com>
2023-07-12 16:51:26 +0200

    abc

---
+++ codeboxapi/box/basebox.py
@@ -0,0 +1,94 @@
+from uuid import uuid4
+from datetime import datetime
+from codeboxapi import settings
+from asyncer import runnify
+from typing_extensions import Self
+from abc import ABC
+from ..utils import base_request
+from ..schema import (
+    CodeBoxStatus, 
+    CodeBoxOutput,
+    CodeBoxFile,
+)
+
+
+class BaseBox(ABC):
+    """ 
+    ABC for Isolated Execution Environments
+    """
+    
+    def __init__(self) -> None:
+        self.id = uuid4().int
+        self.last_interaction = datetime.now()
+
+    def _update(self) -> None:
+        self.last_interaction = datetime.now()
+    
+    def start(self) -> CodeBoxStatus:
+        ...
+    
+    async def astart(self) -> CodeBoxStatus:
+        ...
+        
+    def status(self) -> CodeBoxStatus:
+        ...
+    
+    async def astatus(self) -> CodeBoxStatus:
+        ...
+        
+    def run(self, code: str) -> CodeBoxOutput:
+        ...
+    
+    async def arun(self, code: str) -> CodeBoxOutput:
+        ...
+        
+    def upload(self, file_name: str, content: bytes) -> CodeBoxStatus:
+        ...
+    
+    async def aupload(self, file_name: str, content: bytes) -> CodeBoxStatus:
+        ...
+    
+    def download(self, file_name: str) -> CodeBoxFile:
+        ...
+    
+    async def adownload(self, file_name: str) -> CodeBoxFile:
+        ...
+    
+    def install(self, package_name: str) -> CodeBoxStatus:
+        ...
+    
+    async def ainstall(self, package_name: str) -> CodeBoxStatus:
+        ...
+    
+    def list_files(self) -> list[CodeBoxFile]:
+        ...
+    
+    async def alist_files(self) -> list[CodeBoxFile]:
+        ...
+    
+    def restart(self) -> CodeBoxStatus:
+        ...
+        
+    async def arestart(self) -> CodeBoxStatus:
+        ...
+    
+    def stop(self) -> CodeBoxStatus:
+        ...
+    
+    async def astop(self) -> CodeBoxStatus:
+        ...
+        
+    def __enter__(self) -> Self:
+        self.start()
+        return self
+    
+    async def __aenter__(self) -> Self:
+        await self.astart()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.stop()
+        
+    async def __aexit__(self, exc_type, exc_value, traceback) -> None:
+        await self.astop()
+
