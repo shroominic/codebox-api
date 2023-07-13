5dfb94523560343fae129844dc68aedbacf419a2
Shroominic <db2002dominic@gmail.com>
2023-07-13 11:50:39 +0200

    update schema

--- codeboxapi/box/codebox.py
+++ codeboxapi/box/codebox.py
@@ -1,10 +1,13 @@
-from uuid import uuid4
 from typing import Any
-from datetime import datetime
 from codeboxapi import settings
 from codeboxapi.box import BaseBox
 from ..utils import base_request
-from ..schema import CodeBoxStatus, CodeBoxOutput
+from ..schema import (
+    CodeBoxStatus, 
+    CodeBoxOutput,
+    CodeBoxFile
+)
+from aiohttp import ClientSession
 
 
 class CodeBox(BaseBox):
@@ -19,83 +22,108 @@ class CodeBox(BaseBox):
         else:
             return super().__new__(cls, *args, **kwargs)
     
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.session: ClientSession | None = None
+    
+    def start(self) -> CodeBoxStatus:
+        return self.status()
+    
     async def astart(self) -> CodeBoxStatus:
-        # return self.codebox_request(
-        #     method="POST",  # TODO: add start endpoint
-        #     endpoint=f"/start",
-        # )
+        self.session = ClientSession()
         return await self.astatus()
     
-    async def codebox_request(
+    def codebox_request(
         self, 
         method, 
         endpoint, 
-        body=None, 
-        files=None,
-        content_type="application/json",
+        *args, **kwargs
     ) -> dict[str, Any]:
         self._update()
         return base_request(
-            method=method,
-            endpoint=f"/codebox/{self.id//32**10}" + endpoint,
-            body=body,
-            files=files,
-            content_type=content_type,
+            method,
+            f"/codebox/{self.id}" + endpoint,
+            *args, **kwargs
         )
     
-    async def astatus(self):
+    async def acodebox_request(
+        self,
+        method,
+        endpoint,
+        *args, **kwargs
+    ) -> dict[str, Any]:
+        self._update() # TODO maybe async update?
+        return await abase_request(
+            method,
+            f"/codebox/{self.id}" + endpoint,
+            *args, **kwargs
+        )
+    
+    def status(self):
         return CodeBoxStatus(
-            ** await self.codebox_request(
+            ** self.codebox_request(
                 method="GET",
                 endpoint="/",
             )
         )
         
-    async def arun(self, code: str):
+    def run(self, code: str):
         return CodeBoxOutput(
-            ** await self.codebox_request(
+            ** self.codebox_request(
                 method="POST",
                 endpoint=f"/run",
                 body={"code": code},
             )
         )
         
-    async def aupload_file(self, name: str, content: bytes) -> dict:
-        return await self.codebox_request(
-            method="POST",
-            endpoint="/upload",
-            files={"file": (name, content)},
-            content_type=None
-        )
+    def upload(
+            self, 
+            file_name: str, 
+            content: bytes
+        ) -> CodeBoxStatus:
+        return CodeBoxStatus(
+            ** self.codebox_request(
+                method="POST",
+                endpoint="/upload",
+                files={"file": (file_name, content)},
+                content_type=None
+            ) 
+        )  # TODO: check if schema fits
     
-    async def adownload_file(self, file_name: str) -> dict:
-        return await self.codebox_request(
-            method="GET",
-            endpoint="/download",
-            body={"file_name": file_name},
-            content_type=None,
+    def download(self, file_name: str) -> CodeBoxFile:
+        return CodeBoxFile(
+            ** self.codebox_request(
+                method="GET",
+                endpoint="/download",
+                body={"file_name": file_name},
+                content_type=None,
+            )
         )
         
-    async def ainstall_package(self, package_name: str) -> dict:
-        return await self.codebox_request(
-            method="POST",
-            endpoint="/install",
-            body={
-                "package_name": package_name,
-            },
+    def install(self, package_name: str) -> CodeBoxStatus:
+        return CodeBoxStatus(
+            ** self.codebox_request(
+                method="POST",
+                endpoint="/install",
+                body={
+                    "package_name": package_name,
+                },
+            )
         )
     
-    async def aget_available_files(self) -> list[str]:
-        return (
-            await self.codebox_request(
-                method="GET",
-                endpoint="/files",
-            )
-        )["files"]
+    def list_files(self) -> list[CodeBoxFile]:
+        return [
+            CodeBoxFile(name=file_name, content=None) 
+            for file_name in (
+                self.codebox_request(
+                    method="GET",
+                    endpoint="/files",
+                ))["files"]
+            ]
     
-    async def astop(self) -> CodeBoxStatus:
+    def stop(self) -> CodeBoxStatus:
         return CodeBoxStatus(
-                ** await self.codebox_request(
+            ** self.codebox_request(
                 method="POST",
                 endpoint="/stop",
             )
