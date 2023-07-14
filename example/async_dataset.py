e3351c04ea500b7824875be3e0caf02128ff62b3
Shroominic <db2002dominic@gmail.com>
2023-07-15 01:29:44 +0200

    async dataset test

---
+++ example/async_dataset.py
@@ -0,0 +1,73 @@
+import requests  # type: ignore
+from codeboxapi import CodeBox
+
+
+async def main():
+    async with CodeBox() as codebox:
+        # download the iris dataset
+        csv_bytes = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data").content
+        
+        print("downloaded dataset")
+        
+        # upload the dataset to the codebox
+        o = await codebox.aupload("iris.csv", csv_bytes)
+
+        # dataset analysis code
+        code = """
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+        df = pd.read_csv("iris.csv", header=None)
+        df.columns = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+
+        # Create a color dictionary for each class label
+        color_dict = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+
+        # Map the class labels to numbers
+        df['color'] = df['class'].map(color_dict)
+
+        df.plot.scatter(x="sepal_length", y="sepal_width", c="color", colormap="viridis")
+        plt.show()
+        """
+
+        # run the code
+        output = await codebox.arun(code)
+        print(output.type)
+
+        if output.type == "image/png":
+            # Convert the image content into an image
+            from io import BytesIO
+            import base64
+            
+            try:
+                from PIL import Image  # type: ignore
+            except ImportError:
+                print("Please install it with `pip install codeboxapi[image_support]` to display images.")
+                exit(1)
+            
+            # Decode the base64 string into bytes
+            img_bytes = base64.b64decode(output.content)
+
+            # Create a BytesIO object
+            img_io = BytesIO(img_bytes)
+            
+            # Use PIL to open the image
+            img = Image.open(img_io)
+            
+            # Display the image
+            img.show()
+
+        elif output.type == "error":
+            # error output
+            print("Error:")
+            print(output.content)
+            
+        else:
+            # normal text output
+            print("Text Output:")
+            print(output.content)
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
