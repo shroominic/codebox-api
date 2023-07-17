3cf85921063556f5b67ec47ee4eaf4a9391f44e4
Shroominic <db2002dominic@gmail.com>
2023-07-17 18:20:43 +0200

    remove root aiosession

--- codeboxapi/__init__.py
+++ codeboxapi/__init__.py
@@ -1,22 +1,10 @@
-from contextvars import ContextVar
-from typing import Optional, TYPE_CHECKING
-
 from codeboxapi.config import settings
 from codeboxapi.utils import set_api_key
 from codeboxapi.box.codebox import CodeBox
-
-if TYPE_CHECKING:
-    from aiohttp import ClientSession
-
-
-aiosession: ContextVar[Optional["ClientSession"]] = ContextVar(
-    "aiohttp-session", default=None
-)
 
 
 __all__ = [
     "CodeBox", 
     "set_api_key", 
     "settings",
-    "aiosession"
 ]
