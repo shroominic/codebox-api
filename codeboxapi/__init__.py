286e2ee09613475a8ae24fe4d52ef92c71db9fe8
Shroominic <db2002dominic@gmail.com>
2023-07-27 20:14:41 +0200

    🪚 rm unused imports

--- codeboxapi/__init__.py
+++ codeboxapi/__init__.py
@@ -1,20 +1,7 @@
-from contextvars import ContextVar
-from pathlib import Path
-from typing import Optional, TYPE_CHECKING
-
 from codeboxapi.config import settings
 from codeboxapi.utils import set_api_key
 from codeboxapi.box.codebox import CodeBox
 
-if TYPE_CHECKING:
-    from aiohttp import ClientSession
-
-
-aiosession: ContextVar[Optional["ClientSession"]] = ContextVar(
-    "aiohttp-session", default=None
-)
-
-FILES_DIR = Path(__file__).parent / "files"
 
 __all__ = [
     "CodeBox", 
