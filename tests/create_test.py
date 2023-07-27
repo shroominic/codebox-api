aaaad8cbd27575534cea412f17fe4c2464ea1a57
Shroominic <db2002dominic@gmail.com>
2023-07-27 20:13:48 +0200

    📠 rm comments

--- tests/create_test.py
+++ tests/create_test.py
@@ -2,20 +2,7 @@ import codeboxapi as cb
 
 
 def test_create_codebox():
-    codebox = cb.CodeBox(
-        # Config Ideas:
-        # name="Test Codebox",
-        # python_version="3.8",
-        # requirements=["numpy==1.19.5"],
-        # storage="persistent",
-        # storage_size=100,
-        # vector_store=True,
-        # timeout=15,  # minutes
-        # cpu=1,  # vCPU
-        # memory=512,  # MB
-        # gpu=0,  # GPU
-        # metadata={"key": "value"},
-    )
+    codebox = cb.CodeBox()
     status = codebox.start()
     assert status.status == "started"
 
