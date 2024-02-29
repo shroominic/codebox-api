21ae710f4a21f0db67fdcdcd7633dcf8266e5bea
Shroominic <db2002dominic@gmail.com>
2024-02-29 14:04:44 +0700

    🚚 Refactor import paths

--- src/codeboxapi/__init__.py
+++ src/codeboxapi/__init__.py
@@ -5,12 +5,10 @@ The `codeboxapi` package provides a Python API Wrapper for the Codebox API.
 The package includes modules for configuring the client, setting the API key,
 and interacting with Codebox instances.
 """
-from codeboxapi.box.codebox import CodeBox
-from codeboxapi.config import settings
-from codeboxapi.utils import set_api_key
+from .box import CodeBox
+from .config import settings
 
 __all__ = [
     "CodeBox",
-    "set_api_key",
     "settings",
 ]
