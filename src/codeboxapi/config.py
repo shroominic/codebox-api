cbffe53dd975cef866e2fc2c4aaa719b25637692
Shroominic <db2002dominic@gmail.com>
2024-03-02 16:06:07 +0700

    ✨ Add default_working_dir config

--- src/codeboxapi/config.py
+++ src/codeboxapi/config.py
@@ -15,6 +15,7 @@ class CodeBoxSettings(BaseSettings):
     show_info: bool = True
 
     api_key: str = "local"
+    default_working_dir: str = ".codebox"
     base_url: str = "https://codeboxapi.com/api/v1"
     timeout: int = 20
 
