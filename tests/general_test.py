72094d76b544d489767f44f565b5fd3af1e59635
Shroominic <db2002dominic@gmail.com>
2024-05-09 14:03:27 +0200

    ✅ Enhance test coverage for codebox API with additional assertions and print statements

--- tests/general_test.py
+++ tests/general_test.py
@@ -19,18 +19,22 @@ def run_sync(codebox: CodeBox) -> bool:
     try:
         assert codebox.start() == "started"
         print(codebox.status())
+        print("Started")
 
         assert codebox.status() == "running"
         print(codebox.status())
+        print("Running")
 
         codebox.run("x = 'Hello World!'")
-
         assert codebox.run("print(x)") == "Hello World!\n"
+        print("Printed")
 
         file_name = "test_file.txt"
         assert file_name in str(codebox.upload(file_name, b"Hello World!"))
+        print("Uploaded")
 
         assert codebox.download(file_name).content == b"Hello World!"
+        print("Downloaded")
 
         assert "matplotlib" in str(codebox.install("matplotlib"))
 
@@ -38,15 +42,18 @@ def run_sync(codebox: CodeBox) -> bool:
             "error"
             != codebox.run("import matplotlib; print(matplotlib.__version__)").type
         )
+        print("Installed")
 
         o = codebox.run(
             "import matplotlib.pyplot as plt;"
             "plt.plot([1, 2, 3, 4], [1, 4, 2, 3]); plt.show()"
         )
         assert o.type == "image/png"
+        print("Plotted")
 
     finally:
         assert codebox.stop() == "stopped"
+        print("Stopped")
 
     return True
 
@@ -55,17 +62,22 @@ async def run_async(codebox: CodeBox) -> bool:
     try:
         assert await codebox.astart() == "started"
         print(await codebox.astatus())
+        print("Started")
 
         assert await codebox.astatus() == "running"
         print(await codebox.astatus())
+        print("Running")
 
         await codebox.arun("x = 'Hello World!'")
         assert await codebox.arun("print(x)") == "Hello World!\n"
+        print("Printed")
 
         file_name = "test_file.txt"
         assert file_name in str(await codebox.aupload(file_name, b"Hello World!"))
+        print("Uploaded")
 
         assert (await codebox.adownload(file_name)).content == b"Hello World!"
+        print("Downloaded")
 
         assert "matplotlib" in str(await codebox.ainstall("matplotlib"))
         assert (
@@ -74,15 +86,18 @@ async def run_async(codebox: CodeBox) -> bool:
                 await codebox.arun("import matplotlib; print(matplotlib.__version__)")
             ).type
         )
+        print("Installed")
 
         o = await codebox.arun(
             "import matplotlib.pyplot as plt;"
             "plt.plot([1, 2, 3, 4], [1, 4, 2, 3]); plt.show()"
         )
         assert o.type == "image/png"
+        print("Plotted")
 
     finally:
         assert await codebox.astop() == "stopped"
+        print("Stopped")
 
     return True
 
