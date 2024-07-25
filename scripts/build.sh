4f5ff810135addbc55ae78952ca45baa1c7764ca
Shroominic <db2002dominic@gmail.com>
2024-07-25 17:33:36 +0200

    🐋 build tag push

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,8 @@
 TAG=${1:-latest}
 
-rye build --wheel -c -q
-
 docker build -t codebox .
 
 # todo move container to seperate codeboxapi account
 docker tag codebox:latest shroominic/codebox:$TAG
 
-# docker push shroominic/codebox:$TAG
+docker push shroominic/codebox:$TAG
