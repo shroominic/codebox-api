f8b96fbd5c0a78cef9b88ee3034eaf36c61c47af
Shroominic <db2002dominic@gmail.com>
2024-10-24 10:25:34 +0800

    fix codebox docker

--- src/codeboxapi/docker.py
+++ src/codeboxapi/docker.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import time
 
 import httpx
 
@@ -30,28 +31,36 @@ class DockerBox(RemoteBox):
         self,
         port_or_range: int | tuple[int, int] = 8069,
         image: str = "shroominic/codebox:latest",
-        timeout: float = 1,  # minutes
+        timeout: float = 3,  # minutes
+        start_container: bool = True,
         **_,
     ) -> None:
-        self.port = get_free_port(port_or_range)
-        subprocess.run(
-            [
-                "docker",
-                "run",
-                "-d",
-                "--rm",
-                "-e",
-                f"CODEBOX_TIMEOUT={timeout}",
-                "-p",
-                f"{self.port}:8000",
-                image,
-            ],
-            check=True,
-        )
+        if start_container:
+            self.port = get_free_port(port_or_range)
+            subprocess.run(
+                [
+                    "docker",
+                    "run",
+                    "-d",
+                    "--rm",
+                    "-e",
+                    f"CODEBOX_TIMEOUT={timeout}",
+                    "-p",
+                    f"{self.port}:8069",
+                    image,
+                ],
+                check=True,
+            )
+        else:
+            assert isinstance(port_or_range, int)
+            self.port = port_or_range
         self.session_id = str(self.port)
         self.base_url = f"http://localhost:{self.port}"
         self.client = httpx.Client(base_url=self.base_url)
         self.aclient = httpx.AsyncClient(base_url=self.base_url)
+        self.api_key = "docker"
+        self.factory_id = image
+        self.session_id = str(self.port)
         self._wait_for_startup()
 
     def _wait_for_startup(self) -> None:
@@ -60,4 +69,4 @@ class DockerBox(RemoteBox):
                 self.client.get("/")
                 break
             except httpx.HTTPError:
-                pass
+                time.sleep(1)
