6e421340f1a83a785d73e73016e518020ab80229
Shroominic <db2002dominic@gmail.com>
2024-10-26 15:33:38 +0800

    improve docker build tagging

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,16 @@
-TAG=${1:-latest}
+if [ -z "$1" ]; then
+  echo "Error: No version supplied"
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+VERSION=$1
 
 docker build -t codebox .
 
 # todo move container to seperate codeboxapi account
-docker tag codebox:latest shroominic/codebox:$TAG
+docker tag codebox:latest shroominic/codebox:$VERSION
+docker tag codebox:latest shroominic/codebox:latest
 
-docker push shroominic/codebox:$TAG
+docker push shroominic/codebox:$VERSION
+docker push shroominic/codebox:latest
