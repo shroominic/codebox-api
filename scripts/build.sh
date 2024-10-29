c46f0b30d6e5816d62d1acf62f177df7270da08f
Shroominic <db2002dominic@gmail.com>
2024-10-29 17:13:17 +0800

    rm latest tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,8 +9,8 @@ VERSION=$1
 docker build -t codebox .
 
 # todo move container to seperate codeboxapi account
-docker tag codebox:latest shroominic/codebox:$VERSION
-docker tag codebox:latest shroominic/codebox:latest
+docker tag codebox shroominic/codebox:$VERSION
+docker tag codebox shroominic/codebox:latest
 
 docker push shroominic/codebox:$VERSION
 docker push shroominic/codebox:latest
